87a90a9cf5a37d234426ec57ed3eb18022b1efde
Savor d'Isavano <newelevenken@163.com>
2015-08-26 19:29:45 +0800

    CPU usage calculation

--- src/Core.hs
+++ src/Core.hs
@@ -5,8 +5,9 @@ module Core (
   readStats
   ) where
 
-import qualified Data.ByteString.Lazy       as L
-import qualified Data.ByteString.Lazy.Char8 as C8L
+import           Control.Monad         (liftM)
+import qualified Data.ByteString       as BS
+import qualified Data.ByteString.Char8 as C8L
 
 {-
 A cpu stat consists of the following data:
@@ -30,14 +31,14 @@ data Stat = Stat {
 statFile :: String
 statFile = "/proc/stat"
 
-loadProcStat :: IO L.ByteString
-loadProcStat = L.readFile statFile
+loadProcStat :: IO BS.ByteString
+loadProcStat = BS.readFile statFile
 
-readStats :: L.ByteString -> [Stat]
+readStats :: BS.ByteString -> [Stat]
 readStats bs =
   map makeStat $ takeWhile (C8L.isPrefixOf (C8L.pack "cpu") . head . C8L.words) $ tail $ C8L.split '\n' bs
   where
-    makeStat :: L.ByteString -> Stat
+    makeStat :: BS.ByteString -> Stat
     makeStat s = let
       ws = C8L.words s
       args = map (read . C8L.unpack) $ (take 10 . tail) ws :: [Integer]
@@ -45,6 +46,22 @@ readStats bs =
       in
         Stat suser snice ssystem sidle siowait sirq ssirq ssteal sguest sgnice
 
-readCoreCount :: L.ByteString -> Int
+readCurrentStats :: IO [Stat]
+readCurrentStats = readStats <$> loadProcStat
+
+readCoreCount :: BS.ByteString -> Int
 readCoreCount = length . readStats
 
+calcCPUUsageBetweenStats :: (Fractional a) => Stat -> Stat -> a
+calcCPUUsageBetweenStats prev curr = let
+  (prevBusy, prevIdle) = getBusyIdle prev
+  (currBusy, currIdle) = getBusyIdle curr
+  busyd = currBusy - prevBusy
+  idled = currIdle - prevIdle
+  in
+    fromIntegral busyd / fromIntegral (busyd + idled)
+  where
+    getBusyIdle stat = (
+      statUser stat + statNice stat + statSystem stat + statIRQ stat + statSoftIRQ stat + statSteal stat,
+      statIdle stat + statIOWait stat
+                       )

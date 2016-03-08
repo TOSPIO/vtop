eecef67d25aba071c7ccb22aad03f716622cc10a
Savor d'Isavano <anohigisavay@gmail.com>
2016-03-08 17:59:09 +0800

    Forgot

--- src/Core.hs
+++ src/Core.hs
@@ -5,9 +5,13 @@ module Core (
   readStats
   ) where
 
-import           Control.Monad         (liftM)
+import qualified Control.Concurrent.Timer as Timer
 import qualified Data.ByteString       as BS
 import qualified Data.ByteString.Char8 as C8L
+import           Data.Default
+import           Data.IORef
+import           System.IO.Unsafe
+import           Data.Int
 
 {-
 A cpu stat consists of the following data:
@@ -16,17 +20,28 @@ cpu  74608   2520   24433   1117073   6176   4054  0        0      0      0
 -}
 
 data Stat = Stat {
-  statUser      :: Integer,
-  statNice      :: Integer,
-  statSystem    :: Integer,
-  statIdle      :: Integer,
-  statIOWait    :: Integer,
-  statIRQ       :: Integer,
-  statSoftIRQ   :: Integer,
-  statSteal     :: Integer,
-  statGuest     :: Integer,
-  statGuestNice :: Integer
+  statUser      :: Int64,
+  statNice      :: Int64,
+  statSystem    :: Int64,
+  statIdle      :: Int64,
+  statIOWait    :: Int64,
+  statIRQ       :: Int64,
+  statSoftIRQ   :: Int64,
+  statSteal     :: Int64,
+  statGuest     :: Int64,
+  statGuestNice :: Int64
   } deriving (Show)
+
+defaultStat :: Stat
+defaultStat = Stat 0 0 0 0 0 0 0 0 0 0
+
+defaultDelay :: Int64
+defaultDelay = 1000
+
+type StatPair = (Stat, Stat)
+
+push :: StatPair Stat
+push (l, r) n = (r, n)
 
 statFile :: String
 statFile = "/proc/stat"
@@ -41,7 +56,7 @@ readStats bs =
     makeStat :: BS.ByteString -> Stat
     makeStat s = let
       ws = C8L.words s
-      args = map (read . C8L.unpack) $ (take 10 . tail) ws :: [Integer]
+      args = map (read . C8L.unpack) $ (take 10 . tail) ws :: [Int64]
       suser:snice:ssystem:sidle:siowait:sirq:ssirq:ssteal:sguest:sgnice:_ = args
       in
         Stat suser snice ssystem sidle siowait sirq ssirq ssteal sguest sgnice
@@ -65,3 +80,12 @@ calcCPUUsageBetweenStats prev curr = let
       statUser stat + statNice stat + statSystem stat + statIRQ stat + statSoftIRQ stat + statSteal stat,
       statIdle stat + statIOWait stat
                        )
+
+
+{-# NOINLINE timeSlice #-}
+timeSlice :: IORef Int64
+timeSlice = unsafePerformIO $ newIORef def
+
+incubateStats :: IO [[Stat]]
+incubateStats = do
+  repeatedTimer (msDelay defaultDelay)
